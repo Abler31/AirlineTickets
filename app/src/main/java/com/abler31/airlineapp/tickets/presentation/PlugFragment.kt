87e534a07352ef81c921118337717666223dbe10
Abler31 <58187511+Abler31@users.noreply.github.com>
2024-04-20 16:18:03 +0300

    dialog buttons functionality implemented

---
+++ app/src/main/java/com/abler31/airlineapp/tickets/presentation/PlugFragment.kt
@@ -0,0 +1,20 @@
+package com.abler31.airlineapp.tickets.presentation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.abler31.airlineapp.R
+class PlugFragment : Fragment(R.layout.fragment_plug) {
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+
+
+
+}
--- app/src/main/java/com/abler31/airlineapp/tickets/presentation/TicketsFragment.kt
+++ app/src/main/java/com/abler31/airlineapp/tickets/presentation/TicketsFragment.kt
@@ -4,18 +4,26 @@ import android.app.Dialog
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.Gravity
 import android.view.View
 import android.view.ViewGroup
 import android.view.Window
+import android.view.inputmethod.EditorInfo
+import android.widget.EditText
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.cardview.widget.CardView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.abler31.airlineapp.R
 import com.abler31.airlineapp.tickets.domain.Resource
+import kotlinx.coroutines.delay
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import org.w3c.dom.Text
 
@@ -26,9 +34,9 @@ class TicketsFragment : Fragment(R.layout.fragment_tickets) {
     lateinit var ticketsRecyclerView: RecyclerView
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val searchCardView = view.findViewById<CardView>(R.id.cv_search_view)
         val tvFrom = view.findViewById<TextView>(R.id.et_search_from)
         val tvTo = view.findViewById<TextView>(R.id.et_search_to)
+
         ticketsRecyclerAdapter = TicketsRecyclerAdapter()
         ticketsRecyclerView = view.findViewById(R.id.rv_tickets)
         ticketsRecyclerView.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
@@ -51,23 +59,82 @@ class TicketsFragment : Fragment(R.layout.fragment_tickets) {
         vm.getOffers()
 
         tvFrom.setOnClickListener {
-
             showBottomDialog()
-
         }
-
         tvTo.setOnClickListener {
-
             showBottomDialog()
-
         }
+
 
     }
 
     private fun showBottomDialog(){
         val dialog = Dialog(requireContext())
+
         dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
         dialog.setContentView(R.layout.dialog_search)
+        val searchTo = dialog.findViewById<EditText>(R.id.et_search_to_dialog)
+
+        val difficultRoute = dialog.findViewById<LinearLayout>(R.id.ll_difficult_route)
+        val anywhere = dialog.findViewById<LinearLayout>(R.id.ll_anywhere)
+        val weekends = dialog.findViewById<LinearLayout>(R.id.ll_weekend)
+        val hotWeekdays = dialog.findViewById<LinearLayout>(R.id.ll_hot_weekdays)
+        val istanbul = dialog.findViewById<ConstraintLayout>(R.id.cl_dialog_istanbul)
+        val sochi = dialog.findViewById<ConstraintLayout>(R.id.cl_dialog_sochi)
+        val phuket = dialog.findViewById<ConstraintLayout>(R.id.cl_dialog_phuket)
+
+        difficultRoute.setOnClickListener {
+            findNavController().navigate(R.id.action_navigation_tickets_to_plugFragment)
+            dialog.dismiss()
+        }
+        anywhere.setOnClickListener {
+            searchTo.setText("Стамбул")
+        }
+        weekends.setOnClickListener {
+            findNavController().navigate(R.id.action_navigation_tickets_to_plugFragment)
+            dialog.dismiss()
+        }
+        hotWeekdays.setOnClickListener {
+            findNavController().navigate(R.id.action_navigation_tickets_to_plugFragment)
+            dialog.dismiss()
+        }
+        istanbul.setOnClickListener {
+            searchTo.setText("Стамбул")
+        }
+        sochi.setOnClickListener {
+            searchTo.setText("Сочи")
+        }
+        phuket.setOnClickListener {
+            searchTo.setText("Пхукет")
+        }
+        searchTo.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                searchTo.clearFocus() // Убираем фокус с EditText
+                true
+            } else {
+                false
+            }
+        }
+        searchTo.setOnFocusChangeListener { _, hasFocus ->
+            if (!hasFocus && !searchTo.text.isNullOrBlank()) {
+                dialog.dismiss()
+            }
+        }
+        searchTo.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                if (!searchTo.isFocused && !searchTo.text.isNullOrBlank()) {
+                    dialog.dismiss()
+                }
+            }
+        })
 
         dialog.show()
         dialog.window?.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
