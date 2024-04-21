c77268160e48964883ed7c73b9734f3a316b843d
Abler31 <58187511+Abler31@users.noreply.github.com>
2024-04-21 09:52:23 +0300

    country selected top added

---
+++ app/src/main/java/com/abler31/airlineapp/CountrySelected.kt
@@ -0,0 +1,15 @@
+package com.abler31.airlineapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class CountrySelected : Fragment(R.layout.fragment_country_selected) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
